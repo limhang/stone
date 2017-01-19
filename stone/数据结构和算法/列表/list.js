d70961bb2167ff5555d336061966286702792767
jacob <het@ssiMac.local>
2017-01-19 14:48:43 +0800

    list数据结构初步调通

---
+++ stone/数据结构和算法/列表/list.js
@@ -0,0 +1,61 @@
+function List() {
+	this.listSize = 0;
+	// this.pos = 0;
+	this.dataStore = [];
+	// this.clearList = clearList;
+	this.find = find;
+	this.toString = toString;
+	// this.insert = insert;
+	this.append = append;
+	this.remove = remove;
+	// this.front = front;
+	// this.end = end;
+	// this.prev = prev;
+	// this.nextElement = nextElement;
+	// this.lengthList = List;
+	// this.currPos = currPos;
+	// this.moveTo = moveTo;
+	// this.getElement = getElement;
+}
+
+function append(element) {
+	this.dataStore[this.listSize++] = element;
+}
+
+function find(element) {
+	for(var i = 0; i < this.dataStore.length; ++i) {
+		if (this.dataStore[i] == element) {
+			return i;
+		}
+	}
+	return -1;
+}
+
+function remove(element) {
+	var foundAt = this.find(element);
+	if (foundAt > -1) {
+		this.dataStore.splice(foundAt,1);
+		--this.listSize;
+		return true;
+	}
+	return false;
+}
+
+function lengthlist() {
+	return this.listSize;
+}
+
+function toString() {
+	return this.dataStore;
+}
+
+var names = new List();
+names.append('jacob');
+names.append('raistlin');
+names.append('jetty');
+names.append('bruce');
+// print(names.toString());
+console.log(names.toString());
+names.remove('raistlin');
+// print(names.toString());
+console.log(names.toString());
